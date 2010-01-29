6ebf6ac299fcbebf56f625c7caa10a33c539b321
starkos <unknown>
2010-01-29 18:56:24 +0000

    Added missing SlickEdit action files

---
+++ src/actions/slickedit/_slickedit.lua
@@ -0,0 +1,9 @@
+	premake.slickedit = { }
+	local slickedit = premake.slickedit
+
+	newaction 
+	{
+		trigger         = "slickedit",
+		shortname       = "SlickEdit",
+		description     = "Generate SlickEdit project files (incomplete)",
+	}
