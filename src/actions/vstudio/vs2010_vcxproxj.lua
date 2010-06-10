4484d388a41d69d36e7365d5463cb18b353e2923
LiamDevine <unknown>
2010-06-10 16:45:25 +0100

    vs2010 started vcxproj

---
+++ src/actions/vstudio/vs2010_vcxproxj.lua
@@ -0,0 +1,95 @@
+
+premake.vstudio.vcxproj = { }
+--local vcproj = premake.vstudio.vcxproj
+	
+	local function vs2010_config(prj)		
+		for _, cfginfo in ipairs(prj.solution.vstudio_configs) do
+			--cfginfo = prj.solution.vstudio_configs[1]
+			--local cfg = premake.getconfig(prj, cfginfo.src_buildcfg, cfginfo.src_platform)
+		
+			_p(1,'<ItemGroup Label="ProjectConfigurations">')
+				_p(2,'<ProjectConfiguration Include="%s"', premake.esc(cfginfo.name))
+					_p(3,'<Configuration>%s</Configuration>',cfginfo.buildcfg)
+					_p(3,'<Platform>%s</Platform>',cfginfo.platform)
+				_p(2,'</ProjectConfiguration>')
+			_p(1,'</ItemGroup>')
+		end
+	end
+	
+	local function vs2010_globals(prj)
+		_p(1,'<PropertyGroup Label="Globals">')
+			_p(2,'<ProjectGuid>{%s}</ProjectGuid>',prj.uuid)
+			_p(2,'<RootNamespace>%s</RootNamespace>',prj.name)
+			_p(2,'<Keyword>Win32Proj</Keyword>')
+		_p(1,'</PropertyGroup>')
+	end
+	
+	function config_type(config)
+		local t =
+		{	
+			SharedLib = "DynamicLibrary",
+			StaticLib = "StaticLibrary",
+			ConsoleApp = "Application",
+		}
+		return t[config.kind]
+	end
+	
+	function config_type_block(prj)
+		for _, cfginfo in ipairs(prj.solution.vstudio_configs) do
+			local cfg = premake.getconfig(prj, cfginfo.src_buildcfg, cfginfo.src_platform)
+			_p(1,'<PropertyGroup Condition="\'$(Configuration)|$(Platform)\'==\'%s\'" Label="Configuration">', premake.esc(cfginfo.name))
+				_p(2,'<ConfigurationType>%s</ConfigurationType>',config_type(cfg))
+				_p(2,'<CharacterSet>%s</CharacterSet>',iif(cfg.flags.Unicode,"Unicode","MultiByte"))
+			_p(1,'</PropertyGroup>')
+		end
+	end
+
+	
+	function import_props(prj)
+		for _, cfginfo in ipairs(prj.solution.vstudio_configs) do
+			local cfg = premake.getconfig(prj, cfginfo.src_buildcfg, cfginfo.src_platform)
+			_p(1,'<ImportGroup Condition="\'$(Configuration)|$(Platform)\'==\'%s\'" Label="PropertySheets">',premake.esc(cfginfo.name))
+				_p(2,'<Import Project="$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props" Condition="exists(\'$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props\')" Label="LocalAppDataPlatform" />')
+			_p(1,'</ImportGroup>')
+		end
+	end
+
+--[[
+<PropertyGroup>
+	<_ProjectFileVersion>10.0.30319.1</_ProjectFileVersion>
+	<OutDir Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">..\..\bin\Debug\</OutDir>
+	<IntDir Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">..\..\obj\Debug\string_is_integral\</IntDir>
+	<LinkIncremental Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">true</LinkIncremental>
+	<OutDir Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">..\..\bin\Release\</OutDir>
+	<IntDir Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">..\..\obj\Release\string_is_integral\</IntDir>
+	<LinkIncremental Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">false</LinkIncremental>
+</PropertyGroup>
+--]]
+	
+	function premake.vs2010_vcxproj(prj)
+		io.eol = "\r\n"
+		_p('<?xml version="1.0" encoding="utf-8"?>')
+		_p('<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">')
+			
+			vs2010_config(prj)
+			vs2010_globals(prj)
+			
+			_p(1,'<Import Project="$(VCTargetsPath)\Microsoft.Cpp.Default.props" />')
+			
+			config_type_block(prj)
+			
+			_p(1,'<Import Project="$(VCTargetsPath)\Microsoft.Cpp.props" />')
+			
+			--check what this section is doing
+			_p(1,'<ImportGroup Label="ExtensionSettings">')
+			_p(1,'</ImportGroup>')
+			
+			import_props(prj)
+			
+			--what type of macros are these?
+			_p(1,'<PropertyGroup Label="UserMacros" />')
+
+			
+			
+		_p('</Project>')
+	end
---
+++ tests/actions/vstudio/test_vs2010_vcxproj.lua
@@ -0,0 +1,71 @@
+	T.vs2010_vcxproj = { }
+	local vs10_vcxproj = T.vs2010_vcxproj
+
+--[[
+
+<?xml version="1.0" encoding="utf-8"?>
+<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <ItemGroup Label="ProjectConfigurations">
+    <ProjectConfiguration Include="Debug|Win32">
+      <Configuration>Debug</Configuration>
+      <Platform>Win32</Platform>
+    </ProjectConfiguration>
+    <ProjectConfiguration Include="Release|Win32">
+      <Configuration>Release</Configuration>
+      <Platform>Win32</Platform>
+    </ProjectConfiguration>
+  </ItemGroup>
+</Project>
+--]]
+
+	local sln, prj
+	function vs10_vcxproj.setup()
+		_ACTION = "vs2010"
+
+		sln = solution "MySolution"
+		configurations { "Debug", "Release" }
+		platforms {}
+		
+		--project "DotNetProject"   -- to test handling of .NET platform in solution
+		--language "C#"
+		--kind "ConsoleApp"
+		
+		prj = project "MyProject"
+		language "C++"
+		kind "ConsoleApp"
+		uuid "AE61726D-187C-E440-BD07-2556188A6565"		
+	end
+	
+	local function get_buffer()
+		io.capture()
+		premake.buildconfigs()
+		sln.vstudio_configs = premake.vstudio_buildconfigs(sln)
+		premake.vs2010_vcxproj(prj)
+		buffer = io.endcapture()
+		return buffer
+	end
+
+	function vs10_vcxproj.xmlDeclarationPresent()
+		buffer = get_buffer()
+		test.istrue(string.startswith(buffer, '<?xml version=\"1.0\" encoding=\"utf-8\"?>'))
+	end
+
+	function vs10_vcxproj.projectBlocksArePresent()
+		buffer = get_buffer()
+		test.string_contains(buffer,'<Project*.*</Project>')
+	end
+
+	function vs10_vcxproj.projectOpenTagIsCorrect()
+		buffer = get_buffer()
+		test.string_contains(buffer,'<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">*.*</Project>')
+	end
+	
+	function vs10_vcxproj.configItemGroupPresent()
+		buffer = get_buffer()
+		test.string_contains(buffer,'<ItemGroup Label="ProjectConfigurations2">*.*</ItemGroup>')
+	end
+	
+	function vs10_vcxproj.configBlocksArePresent()
+		buffer = get_buffer()
+		test.string_contains(buffer,'<ProjectConfiguration*.*</ProjectConfiguration>')
+	end
