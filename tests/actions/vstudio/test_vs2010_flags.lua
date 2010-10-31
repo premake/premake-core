6c7968aba6a76cd0a9511615bbccb530c9a506a8
liamDevine <unknown>
2010-10-31 23:24:15 +0000

    adds local keyword to buffer instances

--- tests/actions/vstudio/test_vs2010_flags.lua
+++ tests/actions/vstudio/test_vs2010_flags.lua
@@ -3,12 +3,6 @@ T.vs2010_flags = { }
 local vs10_flags = T.vs2010_flags
 local sln, prj
 
-
---[[
-function vs10_flags.setup()end
-function vs10_flags.nothing() end
---]]
-
 function vs10_flags.setup()
 	_ACTION = "vs2010"
 
@@ -28,7 +22,7 @@ local function get_buffer()
 	premake.buildconfigs()
 	sln.vstudio_configs = premake.vstudio_buildconfigs(sln)
 	premake.vs2010_vcxproj(prj)
-	buffer = io.endcapture()
+	local buffer = io.endcapture()
 	return buffer
 end
 
@@ -36,82 +30,82 @@ end
 function vs10_flags.sseSet()
 	flags  {"EnableSSE"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<EnableEnhancedInstructionSet>StreamingSIMDExtensions</EnableEnhancedInstructionSet>')
 end
 
 function vs10_flags.sse2Set()
 	flags  {"EnableSSE2"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<EnableEnhancedInstructionSet>StreamingSIMDExtensions2</EnableEnhancedInstructionSet>')
 end
 
 function vs10_flags.extraWarningNotSet_warningLevelIsThree()
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<WarningLevel>Level3</WarningLevel>')
 end
 
 function vs10_flags.extraWarning_warningLevelIsFour()
 	flags  {"ExtraWarnings"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<WarningLevel>Level4</WarningLevel>')
 end
 
 function vs10_flags.extraWarning_treatWarningsAsError_setToTrue()
 	flags  {"FatalWarnings"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<TreatWarningAsError>true</TreatWarningAsError>')
 end
 
 function vs10_flags.floatFast_floatingPointModel_setToFast()
 	flags  {"FloatFast"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<FloatingPointModel>Fast</FloatingPointModel>')
 end
 
 function vs10_flags.floatStrict_floatingPointModel_setToStrict()
 	flags  {"FloatStrict"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<FloatingPointModel>Strict</FloatingPointModel>')
 end
 
 function vs10_flags.nativeWideChar_TreatWChar_tAsBuiltInType_setToTrue()
 	flags  {"NativeWChar"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<TreatWChar_tAsBuiltInType>true</TreatWChar_tAsBuiltInType>')
 end
 
 function vs10_flags.nativeWideChar_TreatWChar_tAsBuiltInType_setToFalse()
 	flags  {"NoNativeWChar"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<TreatWChar_tAsBuiltInType>false</TreatWChar_tAsBuiltInType>')
 end
 
 function vs10_flags.noExceptions_exceptionHandling_setToFalse()
 	flags  {"NoExceptions"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<ExceptionHandling>false</ExceptionHandling>')
 end
 
 function vs10_flags.structuredExceptions_exceptionHandling_setToAsync()
 	flags  {"SEH"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<ExceptionHandling>Async</ExceptionHandling>')
 end
 
 function vs10_flags.noFramePointer_omitFramePointers_setToTrue()
 	flags  {"NoFramePointer"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<OmitFramePointers>true</OmitFramePointers>')
 end
 
@@ -119,27 +113,27 @@ end
 function vs10_flags.noRTTI_runtimeTypeInfo_setToFalse()
 	flags  {"NoRTTI"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<RuntimeTypeInfo>false</RuntimeTypeInfo>')
 end
 
 function vs10_flags.optimizeSize_optimization_setToMinSpace()
 	flags  {"OptimizeSize"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<Optimization>MinSpace</Optimization>')
 end
 
 function vs10_flags.optimizeSpeed_optimization_setToMaxSpeed()
 	flags  {"OptimizeSpeed"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<Optimization>MaxSpeed</Optimization>')
 end
 function vs10_flags.optimizeSpeed_optimization_setToMaxSpeed()
 	flags  {"Optimize"}
 		
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<Optimization>Full</Optimization>')
 end
 
@@ -171,14 +165,14 @@ function vs10_flags.releaseAndStaticRuntime_runtimeLibrary_setToMultiThreaded()
 end
 
 function vs10_flags.noCharacterSetDefine_characterSet_setToMultiByte()
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<CharacterSet>MultiByte</CharacterSet>')
 end
 
 function vs10_flags.unicode_characterSet_setToUnicode()
 	flags {"Unicode"}
 	
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<CharacterSet>Unicode</CharacterSet>')
 end
 
@@ -186,58 +180,56 @@ end
 function vs10_flags.noMinimalRebuildYetNotSymbols_minimalRebuild_isNotFound()
 	flags {"NoMinimalRebuild"}
 	
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_does_not_contain(buffer,'MinimalRebuild')
 end
 
 function vs10_flags.noMinimalRebuildAndSymbols_minimalRebuild_setToFalse()
 	flags {"NoMinimalRebuild","Symbols"}
 	
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<MinimalRebuild>false</MinimalRebuild>')
 end
 
 function vs10_flags.symbolsSetYetNotMinimalRebuild_minimalRebuild_setToTrue()
 	flags {"Symbols"}
 	
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<MinimalRebuild>true</MinimalRebuild>')
 end
 
---this generates an error: invalid value 'MFC'
----[[
+
 function vs10_flags.mfc_useOfMfc_setToStatic()
     flags{"MFC"}
     
-    buffer = get_buffer()
+    local buffer = get_buffer()
     test.string_contains(buffer,'<UseOfMfc>Dynamic</UseOfMfc>')
 end
----]]
 
 function vs10_flags.Symbols_DebugInformationFormat_setToEditAndContinue()
 	flags{"Symbols"}
 	
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<DebugInformationFormat>EditAndContinue</DebugInformationFormat>')
 end
 
 function vs10_flags.symbolsAndNoEditAndContinue_DebugInformationFormat_isAnEmptyBlock()
 	flags{"Symbols","NoEditAndContinue"}
 	
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<DebugInformationFormat></DebugInformationFormat>')
 end
 
 function vs10_flags.noManifest_GenerateManifest_setToFalse()
 	flags{"NoManifest"}
 	
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<GenerateManifest Condition="\'%$%(Configuration%)|%$%(Platform%)\'==\'Debug|Win32\'">false</GenerateManifest>')
 end
 
 
 function vs10_flags.programDataBaseFile()
-	buffer = get_buffer()
+	local buffer = get_buffer()
 	test.string_contains(buffer,'<Link>.*<ProgramDataBaseFileName>%$%(OutDir%)MyProject%.pdb</ProgramDataBaseFileName>.*</Link>')
 end
 
