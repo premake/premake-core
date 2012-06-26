b1a2f8918d8ce2b3ed9303af95dc14d246828050
Jason Perkins <starkos@industriousone.com>
2012-06-26 15:10:43 -0400

    Explicitly link dependent project libraries for external (i.e. PS3) VC200x projects to fix intermittent concurrent build issues

--- src/actions/vstudio/vs200x_vcproj.lua
+++ src/actions/vstudio/vs200x_vcproj.lua
@@ -452,7 +452,7 @@
 		end
 			
 		if #cfg.links > 0 then
-			_x(4,'AdditionalDependencies="%s"', table.concat(toolset.getlinks(cfg, true), " "))
+			_x(4,'AdditionalDependencies="%s"', table.concat(toolset.getlinks(cfg), " "))
 		end
 			
 		_x(4,'OutputFile="$(OutDir)\\%s"', config.gettargetinfo(cfg).name)
