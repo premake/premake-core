0df2020f6b43445cdc0bee3ff01fe0ff8ba07e67
Jason Perkins <starkos@industriousone.com>
2014-06-26 10:43:48 -0400

    Pass _PREMAKE_COMMAND unit test when executable uses a non-standard name or location

--- tests/base/test_premake_command.lua
+++ tests/base/test_premake_command.lua
@@ -1,13 +1,12 @@
 --
 -- tests/base/test_premake_command.lua
 -- Test the initialization of the _PREMAKE_COMMAND global.
--- Copyright (c) 2012-2013 Jason Perkins and the Premake project
+-- Copyright (c) 2012-2014 Jason Perkins and the Premake project
 --
 
 	local suite = test.declare("premake_command")
 
 
 	function suite.valueIsSet()
-		local filename = iif(os.is("windows"), "premake5.exe", "premake5")
-		test.isequal(path.getabsolute("../bin/debug/" .. filename), _PREMAKE_COMMAND)
+		test.istrue(os.isfile(_PREMAKE_COMMAND))
 	end
