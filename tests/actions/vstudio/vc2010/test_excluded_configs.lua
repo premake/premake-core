b07b3535073790b40278ec330acf43d4c6d84218
Jason Perkins <starkos@industriousone.com>
2015-07-05 12:51:36 -0400

    Fix unit tests broken by the previous commit

--- tests/actions/vstudio/vc2010/test_excluded_configs.lua
+++ tests/actions/vstudio/vc2010/test_excluded_configs.lua
@@ -71,7 +71,7 @@
 		<Link>
 			<SubSystem>Console</SubSystem>
 			<GenerateDebugInformation>false</GenerateDebugInformation>
-			<AdditionalDependencies>bin/Ares/Debug/MyProject2.lib;%(AdditionalDependencies)</AdditionalDependencies>
+			<AdditionalDependencies>bin\Ares\Debug\MyProject2.lib;%(AdditionalDependencies)</AdditionalDependencies>
 			<EntryPointSymbol>mainCRTStartup</EntryPointSymbol>
 		</Link>
 		<ProjectReference>
