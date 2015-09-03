38ff53daf6199ba1c9211c3f4c18b3f642cbb58d
Jason Perkins <starkos@industriousone.com>
2015-09-03 18:18:04 -0400

    Rename solution to workspace everywhere

--- modules/codelite/tests/test_codelite_workspace.lua
+++ modules/codelite/tests/test_codelite_workspace.lua
@@ -13,17 +13,17 @@
 -- Setup
 --
 
-	local sln, prj
+	local wks, prj
 
 	function suite.setup()
 		_ACTION = "codelite"
 		premake.indent("  ")
-		sln = test.createsolution()
+		wks = test.createWorkspace()
 	end
 
 	local function prepare()
-		sln = test.getsolution(sln)
-		codelite.solution.generate(sln)
+		wks = test.getWorkspace(wks)
+		codelite.workspace.generate(wks)
 	end
 
 
@@ -31,12 +31,12 @@
 -- Check the basic structure of a workspace.
 --
 
-	function suite.onEmptySolution()
-		sln.projects = {}
+	function suite.onEmptyWorkspace()
+		wks.projects = {}
 		prepare()
 		test.capture [[
 <?xml version="1.0" encoding="UTF-8"?>
-<CodeLite_Workspace Name="MySolution" Database="" SWTLW="No">
+<CodeLite_Workspace Name="MyWorkspace" Database="" SWTLW="No">
   <BuildMatrix>
     <WorkspaceConfiguration Name="Debug" Selected="yes">
     </WorkspaceConfiguration>
@@ -47,11 +47,11 @@
 		]]
 	end
 
-	function suite.onDefaultSolution()
+	function suite.onDefaultWorkspace()
 		prepare()
 		test.capture [[
 <?xml version="1.0" encoding="UTF-8"?>
-<CodeLite_Workspace Name="MySolution" Database="" SWTLW="No">
+<CodeLite_Workspace Name="MyWorkspace" Database="" SWTLW="No">
   <Project Name="MyProject" Path="MyProject.project"/>
   <BuildMatrix>
     <WorkspaceConfiguration Name="Debug" Selected="yes">
@@ -66,11 +66,11 @@
 	end
 
 	function suite.onMultipleProjects()
-		test.createproject(sln)
+		test.createproject(wks)
 		prepare()
 		test.capture [[
 <?xml version="1.0" encoding="UTF-8"?>
-<CodeLite_Workspace Name="MySolution" Database="" SWTLW="No">
+<CodeLite_Workspace Name="MyWorkspace" Database="" SWTLW="No">
   <Project Name="MyProject" Path="MyProject.project"/>
   <Project Name="MyProject2" Path="MyProject2.project"/>
   <BuildMatrix>
@@ -97,7 +97,7 @@
 		prepare()
 		test.capture [[
 <?xml version="1.0" encoding="UTF-8"?>
-<CodeLite_Workspace Name="MySolution" Database="" SWTLW="No">
+<CodeLite_Workspace Name="MyWorkspace" Database="" SWTLW="No">
   <Project Name="MyProject" Path="MyProject\MyProject.project"/>
   <BuildMatrix>
     <WorkspaceConfiguration Name="Debug" Selected="yes">
@@ -116,7 +116,7 @@
 		prepare()
 		test.capture [[
 <?xml version="1.0" encoding="UTF-8"?>
-<CodeLite_Workspace Name="MySolution" Database="" SWTLW="No">
+<CodeLite_Workspace Name="MyWorkspace" Database="" SWTLW="No">
   <Project Name="MyProject" Path="..\MyProject\MyProject.project"/>
   <BuildMatrix>
     <WorkspaceConfiguration Name="Debug" Selected="yes">
