7e342a5d0f6af74d38a5b2dbeab97d799b678012
Tom van Dijck <tvandijck@blizzard.com>
2015-09-16 09:07:17 -0700

    Optimize getvpaths. Also sort by length of pattern so the more specific matches match first.

--- src/base/project.lua
+++ src/base/project.lua
@@ -314,55 +314,99 @@
 		-- for tables, each table continuing the path key, which looks up the
 		-- array of paths with should match against that path.
 
-		for _, vpaths in ipairs(prj.vpaths) do
-			for replacement, patterns in pairs(vpaths) do
-				for _, pattern in ipairs(patterns) do
-					local i = abspath:find(path.wildcards(pattern))
-					if i == 1 then
+		local function sort_by_length(a, b)
+			local lena = string.len(a)
+			local lenb = string.len(b)
+			if (lena == lenb) then
+				return a > b
+			end
+			return lena > lenb
+		end
 
-						-- Trim out the part of the name that matched the pattern; what's
-						-- left is the part that gets appended to the replacement to make
-						-- the virtual path. So a pattern like "src/**.h" matching the
-						-- file src/include/hello.h, I want to trim out the src/ part,
-						-- leaving include/hello.h.
+		-- cache patterns.
+		local keys         = prj.cached_vpath_keys
+		local vpaths       = prj.cached_vpaths
+		local wildcards    = prj.cached_wildcards
 
-						-- Find out where the wildcard appears in the match. If there is
-						-- no wildcard, the match includes the entire pattern
+		if not vpaths then
+			vpaths = {}
+			keys = {}
+			wildcards = {}
 
-						i = pattern:find("*", 1, true) or (pattern:len() + 1)
-
-						-- Trim, taking care to keep the actual file name intact.
-
-						local leaf
-						if i < max then
-							leaf = abspath:sub(i)
-						else
-							leaf = fname
-						end
-
-						if leaf:startswith("/") then
-							leaf = leaf:sub(2)
-						end
-
-						-- check for (and remove) stars in the replacement pattern.
-						-- If there are none, then trim all path info from the leaf
-						-- and use just the filename in the replacement (stars should
-						-- really only appear at the end; I'm cheating here)
-
-						local stem = ""
-						if replacement:len() > 0 then
-							stem, stars = replacement:gsub("%*", "")
-							if stars == 0 then
-								leaf = path.getname(leaf)
-							end
-						else
-							leaf = path.getname(leaf)
-						end
-
-						vpath = path.join(stem, leaf)
-						return vpath
+			-- flatten vpaths.
+			for _, v in ipairs(prj.vpaths) do
+				for replacement, patterns in pairs(v) do
+					for _, pattern in ipairs(patterns) do
+						vpaths[pattern] = replacement
 					end
 				end
+			end
+
+			-- sort keys by length,  we want the longest most unique vpaths first.
+			for pattern, _ in pairs(vpaths) do
+				table.insert(keys, pattern)
+			end
+			table.sort(keys, sort_by_length)
+
+			-- cache wildcard results, path.wildcards is expensive!!!
+			for index, pattern in ipairs(keys) do
+				wildcards[index] = path.wildcards(pattern)
+			end
+
+			-- store result.
+			prj.cached_vpath_keys = keys
+			prj.cached_vpaths     = vpaths
+			prj.cached_wildcards  = wildcards
+		end
+
+		-- enumerate vpaths.
+		for index, pattern in ipairs(keys) do
+			local i = abspath:find(wildcards[index])
+			if i == 1 then
+
+				-- Trim out the part of the name that matched the pattern; what's
+				-- left is the part that gets appended to the replacement to make
+				-- the virtual path. So a pattern like "src/**.h" matching the
+				-- file src/include/hello.h, I want to trim out the src/ part,
+				-- leaving include/hello.h.
+
+				-- Find out where the wildcard appears in the match. If there is
+				-- no wildcard, the match includes the entire pattern
+
+				i = pattern:find("*", 1, true) or (pattern:len() + 1)
+
+				-- Trim, taking care to keep the actual file name intact.
+
+				local leaf
+				if i < max then
+					leaf = abspath:sub(i)
+				else
+					leaf = fname
+				end
+
+				if leaf:startswith("/") then
+					leaf = leaf:sub(2)
+				end
+
+				-- check for (and remove) stars in the replacement pattern.
+				-- If there are none, then trim all path info from the leaf
+				-- and use just the filename in the replacement (stars should
+				-- really only appear at the end; I'm cheating here)
+
+				local replacement = vpaths[pattern]
+
+				local stem = ""
+				if replacement:len() > 0 then
+					stem, stars = replacement:gsub("%*", "")
+					if stars == 0 then
+						leaf = path.getname(leaf)
+					end
+				else
+					leaf = path.getname(leaf)
+				end
+
+				vpath = path.join(stem, leaf)
+				return vpath
 			end
 		end
 
