77efd0bab3517efe3b9a28561aad2a58cd1452fa
Sami Kankaristo <sami@indiumgames.fi>
2015-10-01 05:34:46 +0300

    Add basic support for the 'Utility' kind in gmake

--- src/actions/make/make_cpp.lua
+++ src/actions/make/make_cpp.lua
@@ -434,6 +434,9 @@
 			else
 				_p('  LINKCMD = $(AR) -rcs "$@" $(OBJECTS)')
 			end
+		elseif cfg.kind == premake.UTILITY then
+			-- Empty LINKCMD for Utility (only custom build rules)
+			_p('  LINKCMD =')
 		else
 			-- this was $(TARGET) $(LDFLAGS) $(OBJECTS)
 			--   but had trouble linking to certain static libs; $(OBJECTS) moved up
