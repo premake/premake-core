bfd59215cb2a389a224c0a5ac995a5d39ec6527a
Tom van Dijck <tvandijck@blizzard.com>
2015-10-09 10:08:46 -0700

    fix some style and whitespace problems.

--- src/base/os.lua
+++ src/base/os.lua
@@ -603,17 +603,20 @@
 --    True if successful, otherwise nil.
 --
 	function os.copydir(src_dir, dst_dir, filter, single_dst_dir)
-		if not os.isdir(src_dir) then error(src_dir .. " is not an existing directory!") end
+		if not os.isdir(src_dir) then
+			error(src_dir .. " is not an existing directory!")
+		end
+
 		filter = filter or "**"
 		src_dir = src_dir .. "/"
 		print('copy "' .. src_dir .. filter .. '" to "' .. dst_dir .. '".')
 		dst_dir = dst_dir .. "/"
 		local dir = path.rebase(".",path.getabsolute("."), src_dir) -- root dir, relative from src_dir
- 
+
 		os.chdir( src_dir ) -- change current directory to src_dir
-			local matches = os.matchfiles(filter)
+		local matches = os.matchfiles(filter)
 		os.chdir( dir ) -- change current directory back to root
- 
+
 		local counter = 0
 		for k, v in ipairs(matches) do
 			local target = iif(single_dst_dir, path.getname(v), v)
@@ -623,7 +626,7 @@
 				counter = counter + 1
 			end
 		end
- 
+
 		if counter == #matches then
 			print( counter .. " files copied.")
 			return true
