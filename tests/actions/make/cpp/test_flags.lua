696f1ab8cb868115f7a8e58dfc129d88282d34b3
Jason Perkins <starkos@industriousone.com>
2015-10-26 17:03:22 -0400

    Rollback PR #247 - support framework folders in gcc/clang
    Rolled back the change and added a unit test to make sure we catch this in future

--- tests/actions/make/cpp/test_flags.lua
+++ tests/actions/make/cpp/test_flags.lua
@@ -1,7 +1,7 @@
 --
 -- tests/actions/make/cpp/test_flags.lua
 -- Tests compiler and linker flags for Makefiles.
--- Copyright (c) 2012-2013 Jason Perkins and the Premake project
+-- Copyright (c) 2012-2015 Jason Perkins and the Premake project
 --
 
 	local suite = test.declare("make_flags")
