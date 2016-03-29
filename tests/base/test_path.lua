82fd64e00c76caf66d0e90b0faa09350eceddf87
Eyal Solnik <eyalsk@users.noreply.github.com>
2016-03-29 10:02:49 +0300

    Fix getextension to handle paths w/ dot correctly

--- tests/base/test_path.lua
+++ tests/base/test_path.lua
@@ -146,9 +146,17 @@
 	function suite.getextension_ReturnsEmptyString_OnNoExtension()
 		test.isequal("", path.getextension("filename"))
 	end
+    
+    function suite.getextension_ReturnsEmptyString_OnPathWithDotAndNoExtension()
+		test.isequal("", path.getextension("/.premake/premake"))
+	end
 
 	function suite.getextension_ReturnsExtension()
 		test.isequal(".txt", path.getextension("filename.txt"))
+	end
+    
+    function suite.getextension_ReturnsExtension_OnPathWithDot()
+		test.isequal(".lua", path.getextension("/.premake/premake.lua"))
 	end
 
 	function suite.getextension_OnMultipleDots()
