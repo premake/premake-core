e6e50027ef1dc3f3bc6265ec7d9d03488b14b60a
Tom van Dijck <tvandijck@blizzard.com>
2016-06-21 18:01:31 -0700

    Symbols API.

--- src/actions/vstudio/vs2010_vcxproj.lua
+++ src/actions/vstudio/vs2010_vcxproj.lua
@@ -1230,7 +1230,7 @@
 	function m.debugInformationFormat(cfg)
 		local value
 		local tool, toolVersion = p.config.toolset(cfg)
-		if cfg.flags.Symbols then
+		if cfg.symbols == p.ON then
 			if cfg.debugformat == "c7" then
 				value = "OldStyle"
 			elseif cfg.architecture == "x86_64" or
@@ -1243,9 +1243,10 @@
 			else
 				value = "EditAndContinue"
 			end
-		end
-		if value then
-			m.element("DebugInformationFormat", nil, value)
+
+			if value then
+				m.element("DebugInformationFormat", nil, value)
+			end
 		end
 	end
 
@@ -1368,7 +1369,7 @@
 
 
 	function m.generateDebugInformation(cfg)
-		m.element("GenerateDebugInformation", nil, tostring(cfg.flags.Symbols ~= nil))
+		m.element("GenerateDebugInformation", nil, tostring(cfg.symbols == p.ON))
 	end
 
 
