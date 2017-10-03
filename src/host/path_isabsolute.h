e88603d8078601ed6959769fa336430565b6d861
R. Blaine Whittle <bwhittle@blizzard.com>
2017-10-03 16:53:08 -0700

    code style fixes for review of deferred join

---
+++ src/host/path_isabsolute.h
@@ -0,0 +1,9 @@
+/**
+ * \file   path_isabsolute.h
+ * \brief  Determines if a path is absolute or relative.
+ * \author Copyright (c) 2002-2016 Jason Perkins and the Premake project
+ */
+
+#define JOIN_RELATIVE 0
+#define JOIN_ABSOLUTE 1
+#define JOIN_MAYBE_ABSOLUTE 2
