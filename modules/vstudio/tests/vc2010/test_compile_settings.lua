159281ea5af663a86388b6877d5c7c05ccace9ee
Thomas Desveaux <tdesveaux@persistant.fr>
2018-04-10 21:38:31 +0200

    Implement unsignedchar for vstudio

--- modules/vstudio/tests/vc2010/test_compile_settings.lua
+++ modules/vstudio/tests/vc2010/test_compile_settings.lua
@@ -1238,3 +1238,32 @@
 </ClCompile>
 		]]
 	end
+
+--
+-- Check UnsignedChar.
+--
+
+	function suite.unsignedChar_On()
+		unsignedchar "On"
+		prepare()
+		test.capture [[
+<ClCompile>
+	<PrecompiledHeader>NotUsing</PrecompiledHeader>
+	<WarningLevel>Level3</WarningLevel>
+	<Optimization>Disabled</Optimization>
+	<AdditionalOptions>/J %(AdditionalOptions)</AdditionalOptions>
+</ClCompile>
+		]]
+	end
+
+	function suite.unsignedChar_Off()
+		unsignedchar "Off"
+		prepare()
+		test.capture [[
+<ClCompile>
+	<PrecompiledHeader>NotUsing</PrecompiledHeader>
+	<WarningLevel>Level3</WarningLevel>
+	<Optimization>Disabled</Optimization>
+</ClCompile>
+		]]
+	end
