a3301da6197ad4148b2f177d0ff813107f367d0a
Emilio López <redorav@gmail.com>
2018-05-27 14:22:40 +0100

    + Add xbox360 module and delete all references to xbox 360 from the rest of premake, including tests

---
+++ modules/xbox360/xbox360_sln2005.lua
@@ -0,0 +1,10 @@
+--
+-- xbox360/xbox360_sln2005.lua
+-- Xbox 360 integration for vstudio.
+-- Copyright (c) 2012-2015 Manu Evans and the Premake project
+--
+
+	local p = premake
+
+	local xbox360 = p.modules.xbox360
+	local sln2005 = p.vstudio.sln2005
--- tests/config/test_targetinfo.lua
+++ tests/config/test_targetinfo.lua
@@ -185,24 +185,6 @@
 
 
 --
--- Name should use ".exe" for Xbox360 executables.
---
-
-	function suite.nameUsesExe_onXbox360ConsoleApp()
-		kind "ConsoleApp"
-		system "Xbox360"
-		i = prepare()
-		test.isequal("MyProject.exe", i.name)
-	end
-
-	function suite.nameUsesLib_onXbox360StaticLib()
-		kind "StaticLib"
-		system "Xbox360"
-		i = prepare()
-		test.isequal("MyProject.lib", i.name)
-	end
-
---
 -- Name should use a prefix if set.
 --
 
