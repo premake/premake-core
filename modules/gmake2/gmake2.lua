3a8b4b107aaf88763f646eb5ddc3be82e1f67e7f
S6066 <S6066@users.noreply.github.com>
2018-05-31 19:28:12 +0200

    Fix parenthesis again...

--- modules/gmake2/gmake2.lua
+++ modules/gmake2/gmake2.lua
@@ -124,7 +124,7 @@
 	function gmake2.mkdir(dirname)
 		_p('ifeq (posix,$(SHELLTYPE))')
 		_p('\t$(SILENT) mkdir -p %s', dirname)
-		_p('else ifeq(msdos),$(SHELLTYPE))')
+		_p('else ifeq (msdos,$(SHELLTYPE))')
 		_p('\tif not exist "$(subst /,\\\\,%s)" $(SILENT) mkdir $(subst /,\\\\,%s)', dirname, dirname)
 		_p('else')
 		_p('\t$(SILENT) mkdir $(subst /,\\\\,%s)', dirname)
