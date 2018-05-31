7ba491cae6de0de41d2c3e951ba147610622bac9
thomas desveaux <tdesveaux@persistant.fr>
2018-06-01 00:33:08 +0200

    gmake*: fix shell type identification

--- modules/gmake2/gmake2.lua
+++ modules/gmake2/gmake2.lua
@@ -273,12 +273,9 @@
 
 
 	function gmake2.shellType()
-		_p('SHELLTYPE := msdos')
-		_p('ifeq (,$(ComSpec)$(COMSPEC))')
-		_p('  SHELLTYPE := posix')
-		_p('endif')
-		_p('ifeq (/bin,$(findstring /bin,$(SHELL)))')
-		_p('  SHELLTYPE := posix')
+		_p('SHELLTYPE := posix')
+		_p('ifeq (.exe,$(findstring .exe,$(ComSpec)))')
+		_p('\tSHELLTYPE := msdos')
 		_p('endif')
 		_p('')
 	end
