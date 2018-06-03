44c5f8225b23e847d30c07c49da76b3109af2616
Emilio López <redorav@gmail.com>
2018-06-03 14:29:13 +0100

    Fix the licensing and add author

--- modules/xbox360/xbox360_sln2005.lua
+++ modules/xbox360/xbox360_sln2005.lua
@@ -1,7 +1,8 @@
 --
 -- xbox360/xbox360_sln2005.lua
 -- Xbox 360 integration for vstudio.
--- Copyright (c) 2012-2015 Manu Evans and the Premake project
+-- Author: Emilio López
+-- Copyright (c) 2018-2018 Jason Perkins and the Premake project
 --
 
 	local p = premake
