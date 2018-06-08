a05b42169992beb54ae3ae838c5376057a41d8aa
Emilio López <redorav@gmail.com>
2018-06-08 21:51:13 +0100

    Remove the xbox360 module

--- modules/xbox360/_manifest.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"_preload.lua",
-	"xbox360.lua",
-	"xbox360_vcxproj.lua",
-	"xbox360_sln2005.lua",
-	"xbox360_vstudio.lua",
-}
--- modules/xbox360/tests/_tests.lua
+++
@@ -1,6 +0,0 @@
-require ("xbox360")
-
-return {
-	"test_xbox360_project.lua",
-	"test_xbox360_project_vs200x.lua"
-}
