77f6a080cbdf7847ae80997c0da4dc59ba556dbd
Thomas Desveaux <tdesveaux@persistant.fr>
2018-06-18 16:55:26 +0200

    gmake2: Fix perFileFlags generation for cpp + unit test

--- modules/gmake2/gmake2_cpp.lua
+++ modules/gmake2/gmake2_cpp.lua
@@ -569,7 +569,10 @@
 	function cpp.perFileFlags(cfg, fcfg)
 		local toolset = gmake2.getToolSet(cfg)
 
-		local value = gmake2.list(table.join(toolset.getcflags(fcfg), fcfg.buildoptions))
+		local isCFile = path.iscfile(fcfg.name)
+
+		local getflags = iif(isCFile, toolset.getcflags, toolset.getcxxflags)
+		local value = gmake2.list(table.join(getflags(fcfg), fcfg.buildoptions))
 
 		if fcfg.defines or fcfg.undefines then
 			local defs = table.join(toolset.getdefines(fcfg.defines, cfg), toolset.getundefines(fcfg.undefines))
@@ -587,9 +590,9 @@
 
 		if #value > 0 then
 			local newPerFileFlag = false
-			fcfg.flagsVariable, newPerFileFlag = makeVarName(cfg.project, value, iif(path.iscfile(fcfg.name), '_C', '_CPP'))
+			fcfg.flagsVariable, newPerFileFlag = makeVarName(cfg.project, value, iif(isCFile, '_C', '_CPP'))
 			if newPerFileFlag then
-				if path.iscfile(fcfg.name) then
+				if isCFile then
 					_p('%s = $(ALL_CFLAGS)%s', fcfg.flagsVariable, value)
 				else
 					_p('%s = $(ALL_CXXFLAGS)%s', fcfg.flagsVariable, value)
