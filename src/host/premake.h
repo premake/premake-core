4828250a5f06d307f1dcd416af9c5581e9337919
tdesveaux <desveaux.thomas@gmail.com>
2018-09-30 18:19:03 +0200

    Bump PREMAKE_VERSION to alpha13

--- src/host/premake.h
+++ src/host/premake.h
@@ -11,7 +11,7 @@
 
 #include <stdlib.h>
 
-#define PREMAKE_VERSION        "5.0.0-dev"
+#define PREMAKE_VERSION        "5.0.0-alpha13"
 #define PREMAKE_COPYRIGHT      "Copyright (C) 2002-2017 Jason Perkins and the Premake Project"
 #define PREMAKE_PROJECT_URL    "https://github.com/premake/premake-core/wiki"
 
