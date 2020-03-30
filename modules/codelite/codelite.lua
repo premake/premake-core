ecaebbad06ecab7892fe3b3d2091d08041e4c57b
starkos <starkos@industriousone.com>
2020-03-30 15:57:52 -0400

    Fix #1411: Remove "|"  from Codelite config names

--- modules/codelite/codelite.lua
+++ modules/codelite/codelite.lua
@@ -5,8 +5,9 @@
 -- Modified by: Andrea Zanellato
 --              Andrew Gough
 --              Manu Evans
+--              Jason Perkins
 -- Created:     2013/05/06
--- Copyright:   (c) 2008-2015 Jason Perkins and the Premake project
+-- Copyright:   (c) 2008-2020 Jason Perkins and the Premake project
 --
 
 	local p = premake
@@ -21,7 +22,8 @@
 	function codelite.cfgname(cfg)
 		local cfgname = cfg.buildcfg
 		if codelite.workspace.multiplePlatforms then
-			cfgname = string.format("%s|%s", cfg.platform, cfg.buildcfg)
+			-- Codelite breaks if "|" is used here, see #1411
+			cfgname = string.format("%s-%s", cfg.platform, cfg.buildcfg)
 		end
 		return cfgname
 	end
