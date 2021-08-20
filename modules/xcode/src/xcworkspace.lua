f175797da4f32a193b755edc15a958efdd5960ad
starkos <starkos@industriousone.com>
2021-08-20 08:55:59 -0400

    Add xcworkspace project references

--- modules/xcode/src/xcworkspace.lua
+++ modules/xcode/src/xcworkspace.lua
@@ -5,7 +5,9 @@
 local export = require('export')
 local path = require('path')
 local premake = require('premake')
+local xml = require('xml')
 
+local esc = xml.escape
 local wl = export.writeLine
 
 local xcworkspace = {}
@@ -20,7 +22,7 @@ xcworkspace.elements = {
 		return {
 			xcworkspace.xmlDeclaration,
 			xcworkspace.workspace,
-			xcworkspace.fileRefs,
+			xcworkspace.projects,
 			xcworkspace.endTag,
 		}
 	end,
@@ -56,17 +58,37 @@ function xcworkspace.workspace()
 end
 
 
-function xcworkspace.fileRefs()
-	wl([[
-	<FileRef
-		location = "group:Premake.xcodeproj">
-	</FileRef>]])
+function xcworkspace.projects(wks)
+	local projects = wks.projects
+	for i = 1, #projects do
+		xcworkspace.fileRef(projects[i])
+	end
 end
 
 
-function xcworkspace.endTag(prj)
+function xcworkspace.endTag()
 	export.outdent()
 	wl('</Workspace>')
+end
+
+
+---
+-- Handlers for individual setting elements, in alpha order
+---
+
+function xcworkspace.fileRef(prj)
+	-- Paths will arrive like "MyWorkspace.xcworkspace/contents.xcworkspacedata" and
+	-- "MyProject.xcodeproj/project.pbx". Need to trim off the internal file name parts
+	-- in order to get a correct relative path
+	local wksRoot = path.getDirectory(prj.workspace.exportPath)
+	local prjRoot = path.getDirectory(prj.exportPath)
+	local relativePath = path.getRelativeFile(wksRoot, prjRoot)
+
+	wl('<FileRef')
+	export.indent()
+	wl('location = "group:%s">', esc(relativePath))
+	export.outdent()
+	wl('</FileRef>')
 end
 
 
---
+++ modules/xcode/tests/xcworkspace/xcworkspace_projects_tests.lua
@@ -0,0 +1,83 @@
+local premake = require('premake')
+local xcode = require('xcode')
+
+local xcworkspace = xcode.xcworkspace
+
+local XcWksProjectsTests = test.declare('XcWksProjectsTests', 'xcworkspace', 'xcode')
+
+
+local function _execute(fn)
+	workspace('MyWorkspace', function ()
+		configurations({ 'Debug', 'Release' })
+		fn()
+	end)
+
+	local wks = xcode.buildDom().workspaces['MyWorkspace']
+	xcworkspace.projects(wks)
+end
+
+
+---
+-- Test path building to projects in various relative locations
+---
+
+function XcWksProjectsTests.projectInSameFolderAsWorkspace()
+	_execute(function ()
+		project('MyProject')
+	end)
+
+	test.capture [[
+<FileRef
+	location = "group:MyProject.xcodeproj">
+</FileRef>
+	]]
+end
+
+
+function XcWksProjectsTests.projectInSubfolder()
+	_execute(function ()
+		project('MyProject', function ()
+			location('MyProject')
+		end)
+	end)
+
+	test.capture [[
+<FileRef
+	location = "group:MyProject/MyProject.xcodeproj">
+</FileRef>
+	]]
+end
+
+
+function XcWksProjectsTests.projectOutsideWorkspaceFolder()
+	_execute(function ()
+		project('MyProject', function ()
+			location('../MyProject')
+		end)
+	end)
+
+	test.capture [[
+<FileRef
+	location = "group:../MyProject/MyProject.xcodeproj">
+</FileRef>
+	]]
+end
+
+
+---
+-- Check escaping of project names and paths
+---
+
+
+function XcWksProjectsTests.escapesDoubleQuotesInPaths()
+	_execute(function ()
+		project('My "x64" Project')
+	end)
+
+	test.capture [[
+<FileRef
+	location = "group:My &quot;x64&quot; Project.xcodeproj">
+</FileRef>
+	]]
+end
+
