58780d67091b72929ccdf8d5fc79d425c7f6eb23
starkos <starkos@industriousone.com>
2021-10-21 14:14:35 -0600

    Prepare 5.0-beta1 release

--- website/src/pages/download.js
+++ website/src/pages/download.js
@@ -6,7 +6,7 @@ import { Column, Container, Row } from '../components/Grid';
 import Sponsors from '../components/Sponsors';
 
 
-const LATEST_VERSION = '5.0.0-alpha16';
+const LATEST_VERSION = '5.0.0-beta1';
 
 
 const DownloadLink = ({ arch }) => {
