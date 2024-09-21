450d0d674fd9dd810ecf5e38b6c0d29d26681eb1
Joao Matos <joao@tritao.eu>
2024-09-21 11:31:14 +0100

    Updated documentation.

--- website/sidebars.js
+++ website/sidebars.js
@@ -489,10 +489,11 @@ module.exports = {
 					label: 'term',
 					items: [
 						'term.getTextColor',
+						'term.setTextColor',
 						'term.popColor',
 						'term.pushColor',
-						'term.setTextColor',
-						'term.clearToEndOfLine'
+						'term.clearToEndOfLine',
+						'term.moveLeft'
 					]
 				},
 				{
