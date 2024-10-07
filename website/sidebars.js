29fe49ff1fcd7afbc476ba157edbc460152ae318
Beernaert Robbe <robbe.beernaert@student.howest.be>
2024-10-07 19:37:02 +0200

    updated sidebar.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -135,6 +135,7 @@ module.exports = {
 						'docname',
 						'documentationfile',
 						'dotnetframework',
+						'dotnetsdk',
 						'dpiawareness',
 						'editandcontinue',
 						'editorintegration',
