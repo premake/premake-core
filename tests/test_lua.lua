premake/premake-core

commit afcbbb2bb8f1ef79565ab8035f71f59a037d65e1

Fix stack manipulation in Premake's `luaL_loadfilex` override.
An example of code that did not work:

```lua
local env = { ["foo"] = function(n) print(n) end }
assert(pcall(loadfile("foo.lua", nil, env))
```

src/host/lua_auxlib.c
@@ -29,6 +29,10 @@ LUALIB_API int luaL_loadfilex (lua_State* L, const char* filename, const char* m
 	const char* script_dir;
 	const char* test_name;
 
+	/* this function can be called with from 1 to 3 arguments on the stack,
+	 * the filename, the mode and an environment table */
+
+  	int env = (!lua_isnone(L, 3) ? 1 : 0);  /* 1 if there is an env or 0 if no 'env' */
 	int bottom = lua_gettop(L);
 	int z = !OKAY;
 
@@ -60,11 +64,11 @@ LUALIB_API int luaL_loadfilex (lua_State* L, const char* filename, const char* m
 			z = premake_load_embedded_script(L, test_name + 2); /* Skip over leading "$/" */
 
 			/* remove test_name */
-			lua_remove(L, bottom + 1);
+			lua_remove(L, -3);
 		}
 
 		/* remove _SCRIPT_DIR */
-		lua_remove(L, bottom);
+		lua_remove(L, bottom + env);
 	}
 
 	/* Try to locate the script on the filesystem */
@@ -100,7 +104,15 @@ LUALIB_API int luaL_loadfilex (lua_State* L, const char* filename, const char* m
 	 * script chunk on the stack. Turn these into a closure that will call my
 	 * wrapper below when the loaded script needs to be executed. */
 	if (z == OKAY) {
-		lua_pushcclosure(L, chunk_wrapper, 2);
+		/* if we are called with an env, then our caller, luaB_loadfile, will
+		 * call load_aux, which sets up our env as the first up value via
+		 * lua_setupvalue, which would overwrite the one we are setting up here.
+		 * workaround this by pushing a nil value as our first up value */
+		if (env) {
+			lua_pushnil(L);
+			lua_insert(L, -3);
+		}
+		lua_pushcclosure(L, chunk_wrapper, 2 + (env ? 1 : 0));
 	}
 	else if (z == LUA_ERRFILE) {
 		lua_pushfstring(L, "cannot open %s: No such file or directory", filename);
@@ -124,8 +136,13 @@ static int chunk_wrapper(lua_State* L)
 	const char* filename;
 	char* ptr;
 	int i, args;
+	int upvalue_offset;
 
 	args = lua_gettop(L);
+
+	/* if the first up value is a table, then we have an env upvalue
+	 * and should take that into account by offsetting the rest of the up values */
+	upvalue_offset = (lua_type(L, lua_upvalueindex(1)) == LUA_TTABLE) ? 1 : 0;
 
 	/* Remember the current _SCRIPT and working directory so I can
 	 * restore them after this new chunk has been run. */
@@ -136,12 +153,12 @@ static int chunk_wrapper(lua_State* L)
 
 	/* Set the new _SCRIPT variable */
 
-	lua_pushvalue(L, lua_upvalueindex(1));
+	lua_pushvalue(L, lua_upvalueindex(1 + upvalue_offset));
 	lua_setglobal(L, "_SCRIPT");
 
 	/* And the new _SCRIPT_DIR variable (const cheating) */
 
-	filename = lua_tostring(L, lua_upvalueindex(1));
+	filename = lua_tostring(L, lua_upvalueindex(1 + upvalue_offset));
 	ptr = strrchr(filename, '/');
 	if (ptr) *ptr = '\0';
 	lua_pushlstring(L, filename, strlen(filename));
@@ -157,7 +174,15 @@ static int chunk_wrapper(lua_State* L)
 	/* Move the function's arguments to the top of the stack and
 	 * execute the function created by luaL_loadfile() */
 
-	lua_pushvalue(L, lua_upvalueindex(2));
+	lua_pushvalue(L, lua_upvalueindex(2 + upvalue_offset));
+
+	/* forward the env table to the closure as 1st upvalue */
+	if (upvalue_offset) {
+		lua_pushvalue(L, -1);
+	 	lua_pushvalue(L, lua_upvalueindex(1));
+		lua_setupvalue(L, -2, 1);
+	}
+
 	for (i = 1; i <= args; ++i) {
 		lua_pushvalue(L, i);
 	}

tests/test_lua.lua
@@ -1,0 +1,37 @@
+--
+-- tests/test_lua.lua
+-- Automated test suite for Lua base functions.
+-- Copyright (c) 2008 Jason Perkins and the Premake project
+--
+
+	local suite = test.declare("lua")
+
+--
+-- loadfile
+--
+
+function suite.loadfile()
+	local file =  path.join(_SCRIPT_DIR, "test_lua_loaded_noenv.lua")
+	local fn = assert(loadfile(file, nil))
+	local ret, value = pcall(fn)
+	test.isequal(10, value)
+end
+
+
+
+
+--
+-- loadfile with custom env
+--
+
+	function suite.loadfile_with_env()
+		local file =  path.join(_SCRIPT_DIR, "test_lua_loaded.lua")
+		local value = 0
+		local env = {
+			["foobar"] = function(n) value = n end
+		}
+		local fn = assert(loadfile(file, nil, env))
+		pcall(fn)
+		test.isequal(10, value)
+	end
+

tests/_tests.lua
@@ -1,5 +1,6 @@
 return {
 	-- Base API tests
+	"test_lua.lua",
 	"test_string.lua",
 	"base/test_aliasing.lua",
 	"base/test_binmodules.lua",

tests/test_lua_loaded.lua
@@ -1,0 +1,1 @@
+foobar(10)

tests/test_lua_loaded_noenv.lua
@@ -1,0 +1,1 @@
+return 10

commit 7fcf9dd93121c2f136bb10bf74d4bc736250538f

Fix issue with `lua_loadfile` and `require`.
Fixes an regression introduced in
https://github.com/premake/premake-core/commit/afcbbb2bb8f1ef79565ab8035f71f59a037d65e1.

`require` actually follows a different code path in Lua with a different
stack layout expectation than the other code paths.

Fix the code to deal with this case, and also clean up some other
tidbits around cleaning up stack values. The new approach is simpler to
understand and should fix a potential issue that could happen in the
error case.

Fixes https://github.com/premake/premake-core/issues/2314.

src/host/lua_auxlib.c
@@ -5,6 +5,7 @@
  */
 
 #include "premake.h"
+#include <assert.h>
 
 
 static int chunk_wrapper(lua_State* L);
@@ -29,11 +30,15 @@ LUALIB_API int luaL_loadfilex (lua_State* L, const char* filename, const char* m
 	const char* script_dir;
 	const char* test_name;
 
-	/* this function can be called with from 1 to 3 arguments on the stack,
+	/* this function is usually called with from 1 to 3 arguments on the stack,
 	 * the filename, the mode and an environment table */
 
-  	int env = (!lua_isnone(L, 3) ? 1 : 0);  /* 1 if there is an env or 0 if no 'env' */
+	/* however, in the case of require, we end up being called from searcher_Lua,
+	   which sets up extra values on the stack */
 	int bottom = lua_gettop(L);
+	int is_require = bottom >= 4;
+
+  	int env = (!is_require && !lua_isnone(L, 3)) ? 1 : 0;  /* 1 if there is an env or 0 if no 'env' */
 	int z = !OKAY;
 
 	/* If filename starts with "$/" then we want to load the version that
@@ -50,6 +55,7 @@ LUALIB_API int luaL_loadfilex (lua_State* L, const char* filename, const char* m
 	if (z != OKAY) {
 		lua_getglobal(L, "_SCRIPT_DIR");
 		script_dir = lua_tostring(L, -1);
+		int script_dir_index = lua_gettop(L);
 
 		if (script_dir && script_dir[0] == '$') {
 			/* Call `path.getabsolute(filename, _SCRIPT_DIR)` to resolve any
@@ -59,16 +65,17 @@ LUALIB_API int luaL_loadfilex (lua_State* L, const char* filename, const char* m
 			lua_pushvalue(L, -3);
 			lua_call(L, 2, 1);
 			test_name = lua_tostring(L, -1);
+			int test_name_index = lua_gettop(L);
 
 			/* if successful, filename and chunk will be on top of stack */
 			z = premake_load_embedded_script(L, test_name + 2); /* Skip over leading "$/" */
 
 			/* remove test_name */
-			lua_remove(L, -3);
+			lua_remove(L, test_name_index);
 		}
 
 		/* remove _SCRIPT_DIR */
-		lua_remove(L, bottom + env);
+		lua_remove(L, script_dir_index);
 	}
 
 	/* Try to locate the script on the filesystem */
@@ -103,6 +110,9 @@ LUALIB_API int luaL_loadfilex (lua_State* L, const char* filename, const char* m
 	/* Either way I should have ended up with the file name followed by the
 	 * script chunk on the stack. Turn these into a closure that will call my
 	 * wrapper below when the loaded script needs to be executed. */
+
+	assert(lua_gettop(L) == bottom + 2);
+
 	if (z == OKAY) {
 		/* if we are called with an env, then our caller, luaB_loadfile, will
 		 * call load_aux, which sets up our env as the first up value via

tests/folder/init.lua
@@ -1,0 +1,1 @@
+-- empty file used by loadfile_with_require test

tests/test_lua.lua
@@ -35,3 +35,13 @@ end
 		test.isequal(10, value)
 	end
 
+
+--
+-- loadfile via require with init.lua
+--
+
+function suite.loadfile_with_require()
+	os.chdir(_SCRIPT_DIR)
+	assert(require("folder"))
+end
+