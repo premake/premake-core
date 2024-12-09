a29b9497fc1852a94d87872c2fd0fb109b09d804
Emilio López <redorav@gmail.com>
2024-12-09 20:20:25 +0000

    Rename the references, and sort alphabetically

--- modules/vstudio/vstudio.lua
+++ modules/vstudio/vstudio.lua
@@ -661,7 +661,7 @@
 	include("vs2010_rules_props.lua")
 	include("vs2010_rules_targets.lua")
 	include("vs2010_rules_xml.lua")
-	include("vs2010_androidproj.lua")
 	include("vs2013_vcxitems.lua")
+	include("vs2015_androidproj.lua")
 
 	return p.modules.vstudio
