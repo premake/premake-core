5921262fd6274c1591dc0313274c93ba7f54df7c
Samuel Surtees <11887457+samsinsane@users.noreply.github.com>
2024-12-10 15:45:32 +1000

    Added ability to use system lua (#2370)

--- src/host/premake.h
+++ src/host/premake.h
@@ -5,9 +5,15 @@
  */
 
 #define lua_c
+#ifdef LUA_STATICLIB
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
+#else
+#include <lua5.3/lua.h>
+#include <lua5.3/lauxlib.h>
+#include <lua5.3/lualib.h>
+#endif
 
 #include <stdint.h>
 #include <stdlib.h>
