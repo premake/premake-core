7f543234dfa7e0f631a7e3a7b4ffa649468e7666
Nick Clark <nicholaus.clark115@gmail.com>
2024-12-28 11:01:40 -0500

    Release 5.0-beta4 prep

--- website/src/pages/download.js
+++ website/src/pages/download.js
@@ -6,7 +6,7 @@ import { Column, Container, Row } from '../components/Grid';
 import Sponsors from '../components/Sponsors';
 
 
-const LATEST_VERSION = '5.0.0-beta3';
+const LATEST_VERSION = '5.0.0-beta4';
 
 
 const DownloadLink = ({ arch }) => {
