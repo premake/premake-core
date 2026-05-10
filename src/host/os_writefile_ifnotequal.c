premake/premake-core

commit c8c60f8fd47f9c3687ef4a3a6afbeeccc8bb0539

Fix path encoding issues (#1153) (#2654)
* Test Unicode functionality

* Lua support for UTF-8 on Windows

* Premake support for UTF-8

src/host/os_writefile_ifnotequal.c
@@ -10,7 +10,7 @@
 #include <string.h>
 #include "premake.h"
 
-static int compare_file(const char* content, size_t length, const char* dst)
+static int compare_file(lua_State *L, const char* content, size_t length, const char* dst)
 {
 	FILE* file;
 	size_t size;
@@ -18,15 +18,17 @@ static int compare_file(const char* content, size_t length, const char* dst)
 	char buffer[4096];
 	size_t num;
 
-	#if PLATFORM_WINDOWS
-	wchar_t wide_path[PATH_MAX];
-	if (MultiByteToWideChar(CP_UTF8, 0, dst, -1, wide_path, PATH_MAX) == 0)
+#if PLATFORM_WINDOWS
+	const wchar_t *wpath = luaL_convertstring(L, dst);
+	if (!wpath)
 		return FALSE;
 
-	file = _wfopen(wide_path, L"rb");
-	#else
+	file = _wfopen(wpath, L"rb");
+	lua_pop(L, 1);
+#else
+	(void)(L);
 	file = fopen(dst, "rb");
-	#endif
+#endif
 
 	if (file == NULL)
 	{
@@ -78,29 +80,34 @@ int os_writefile_ifnotequal(lua_State* L)
 	const char* dst     = luaL_checkstring(L, 2);
 
 	// if destination exist, and they are the same, no need to copy.
-	if (do_isfile(L, dst) && compare_file(content, length, dst))
+	if (do_isfile(L, dst) && compare_file(L, content, length, dst))
 	{
 		lua_pushinteger(L, 0);
 		return 1;
 	}
 
 	#if PLATFORM_WINDOWS
-	wchar_t wide_path[PATH_MAX];
-	if (MultiByteToWideChar(CP_UTF8, 0, dst, -1, wide_path, PATH_MAX) == 0)
+	const wchar_t *wpath = luaL_convertstring(L, dst);
+	if (!wpath)
 		return FALSE;
 
-	file = _wfopen(wide_path, L"wb");
+	file = _wfopen(wpath, L"wb");
+	lua_pop(L, 1);
 	#else
+	(void)(L);
 	file = fopen(dst, "wb");
 	#endif
 
 	if (file != NULL)
 	{
-		fwrite(content, 1, length, file);
+		int error = fwrite(content, length, 1, file) != 1;
 		fclose(file);
 
-		lua_pushinteger(L, 1);
-		return 1;
+		if (!error)
+		{
+			lua_pushinteger(L, error ? -1 : 0);
+			return 1;
+		}
 	}
 
 	lua_pushinteger(L, -1);

Bootstrap.mak
@@ -106,7 +106,7 @@ mingw-clean: nix-clean
 
 mingw: mingw-clean
 	mkdir -p build/bootstrap
-	$(CC) -o build/bootstrap/premake_bootstrap -DPREMAKE_NO_BUILTIN_SCRIPTS -DLUA_STATICLIB -I"$(LUA_DIR)" -I"$(LUASHIM_DIR)" $(SRC) -lole32 -lversion
+	$(CC) -o build/bootstrap/premake_bootstrap -DPREMAKE_NO_BUILTIN_SCRIPTS -DLUA_STATICLIB -DUNICODE -D_UNICODE -municode -I"$(LUA_DIR)" -I"$(LUASHIM_DIR)" $(SRC) -lole32 -lversion
 	./build/bootstrap/premake_bootstrap embed
 	./build/bootstrap/premake_bootstrap --arch=$(PLATFORM) --os=windows --to=build/bootstrap --cc=gcc $(PREMAKE_OPTS) gmake
 	$(MAKE) -C build/bootstrap -j`getconf _NPROCESSORS_ONLN` config=$(CONFIG)_$(PLATFORM:x86=win32)
@@ -162,7 +162,7 @@ haiku: haiku-clean
 
 windows-base: windows-clean
 	if not exist build\bootstrap (mkdir build\bootstrap)
-	cl /Fo.\build\bootstrap\ /Fe.\build\bootstrap\premake_bootstrap.exe /DPREMAKE_NO_BUILTIN_SCRIPTS /DLUA_STATICLIB /I"$(LUA_DIR)" /I"$(LUASHIM_DIR)" user32.lib ole32.lib advapi32.lib $(SRC)
+	cl /Fo.\build\bootstrap\ /Fe.\build\bootstrap\premake_bootstrap.exe /DPREMAKE_NO_BUILTIN_SCRIPTS /DLUA_STATICLIB /DUNICODE /D_UNICODE /I"$(LUA_DIR)" /I"$(LUASHIM_DIR)" user32.lib ole32.lib advapi32.lib $(SRC)
 	.\build\bootstrap\premake_bootstrap.exe embed
 	.\build\bootstrap\premake_bootstrap --arch=$(PLATFORM) --to=build/bootstrap $(PREMAKE_OPTS) $(MSDEV)
 

binmodules/luasocket/src/inet.c
@@ -12,6 +12,11 @@
 
 #include "inet.h"
 
+#ifdef _WIN32 /* PREMAKE: Fix for unicode Windows build */
+#undef gai_strerror
+#define gai_strerror gai_strerrorA
+#endif
+
 /*=========================================================================*\
 * Internal function prototypes.
 \*=========================================================================*/

binmodules/luasocket/src/udp.c
@@ -23,6 +23,11 @@
 #define MAX(x, y) ((x) > (y) ? x : y)
 #endif
 
+#ifdef _WIN32 /* PREMAKE: Fix for unicode Windows build */
+#undef gai_strerror
+#define gai_strerror gai_strerrorA
+#endif
+
 /*=========================================================================*\
 * Internal function prototypes
 \*=========================================================================*/

binmodules/luasocket/src/wsocket.c
@@ -427,7 +427,7 @@ const char *socket_gaistrerror(int err) {
 #ifdef EAI_SYSTEM
         case EAI_SYSTEM: return strerror(errno);
 #endif
-        default: return gai_strerror(err);
+        default: return gai_strerrorA(err);
     }
 }
 

contrib/lua/src/lauxlib.c
@@ -26,6 +26,9 @@
 
 #include "lauxlib.h"
 
+#if defined(LUA_USE_WINDOWS)  /* PREMAKE: UTF-8 support on Windows */
+#include <windows.h>
+#endif
 
 /*
 ** {======================================================
@@ -706,19 +709,34 @@ LUALIB_API int luaL_loadfilex (lua_State *L, const char *filename,
   int status, readstatus;
   int c;
   int fnameindex = lua_gettop(L) + 1;  /* index of filename on the stack */
+#if defined(LUA_USE_WINDOWS)  /* PREMAKE: UTF-8 character support on windows */
+  const wchar_t *wfilename = NULL;
+#endif
   if (filename == NULL) {
     lua_pushliteral(L, "=stdin");
     lf.f = stdin;
   }
   else {
     lua_pushfstring(L, "@%s", filename);
+#if defined(LUA_USE_WINDOWS)  /* PREMAKE: UTF-8 character support on windows */
+    wfilename = luaL_convertstring(L, filename);
+    lf.f = wfilename ? _wfopen(wfilename, L"r") : NULL;
+    if (wfilename) lua_pop(L, 1);
+#else
     lf.f = fopen(filename, "r");
+#endif
     if (lf.f == NULL) return errfile(L, "open", fnameindex);
   }
   if (skipcomment(&lf, &c))  /* read initial portion */
     lf.buff[lf.n++] = '\n';  /* add line to correct line numbers */
   if (c == LUA_SIGNATURE[0] && filename) {  /* binary file? */
+#if defined(LUA_USE_WINDOWS)  /* PREMAKE: UTF-8 character support on windows */
+    wfilename = luaL_convertstring(L, filename);
+    lf.f = wfilename ? _wfreopen(wfilename, L"rb", lf.f) : NULL;  /* reopen in binary mode */
+    if (wfilename) lua_pop(L, 1);
+#else
     lf.f = freopen(filename, "rb", lf.f);  /* reopen in binary mode */
+#endif
     if (lf.f == NULL) return errfile(L, "reopen", fnameindex);
     skipcomment(&lf, &c);  /* re-read initial portion */
   }
@@ -988,6 +1006,36 @@ LUALIB_API void luaL_requiref (lua_State *L, const char *modname,
 }
 
 
+LUALIB_API const char *luaL_getenv (lua_State *L, const char *name) {
+#if defined(LUA_USE_WINDOWS)
+  const wchar_t *wname = luaL_convertstring(L, name);
+  const char *s;
+  wchar_t *wvar;
+  DWORD rc;
+  luaL_Buffer wbuf;
+  if (wname == NULL) return NULL;
+  rc = GetEnvironmentVariableW(wname, NULL, 0);
+  if (!rc) {
+    lua_pop(L, 1);
+    return NULL;
+  }
+  wvar = (wchar_t *)luaL_buffinitsize(L, &wbuf, rc * sizeof(wchar_t));
+  GetEnvironmentVariableW(wname, wvar, rc);
+  luaL_pushresultsize(&wbuf, rc * sizeof(wchar_t));
+  wvar = (wchar_t *)lua_tostring(L, -1);
+  s = luaL_convertlwstring(L, wvar, rc - 1, NULL);
+  lua_remove(L, -2); /* remove the string from wbuf */
+  lua_remove(L, -2); /* remove the string from wname */
+  return s;
+#else
+  const char *value = getenv(name);
+  if (!value) return NULL;
+  lua_pushstring(L, value);
+  return lua_tostring(L, -1);
+#endif
+}
+
+
 LUALIB_API const char *luaL_gsub (lua_State *L, const char *s, const char *p,
                                                                const char *r) {
   const char *wild;
@@ -1041,3 +1089,116 @@ LUALIB_API void luaL_checkversion_ (lua_State *L, lua_Number ver, size_t sz) {
                   (LUAI_UACNUMBER)ver, (LUAI_UACNUMBER)*v);
 }
 
+/*
+** {======================================================
+** Lua wide-string support (Windows only)
+** =======================================================
+*/
+#if defined(LUA_USE_WINDOWS)
+typedef struct UWideString {
+  size_t len;
+  wchar_t s[1]; /* actual size is len + 1 */
+} UWideString;
+
+#define LUA_WIDESTRING "LUA_WIDESTRING"
+
+static UWideString *newwidestr (lua_State *L, const wchar_t *s, size_t len) {
+  UWideString *ws = (UWideString *)lua_newuserdata(L, sizeof(UWideString) + len * sizeof(wchar_t));
+  luaL_newmetatable(L, LUA_WIDESTRING);
+  lua_setmetatable(L, -2);
+  if (s) memcpy(ws->s, s, len * sizeof(wchar_t));
+  ws->s[len] = L'\0';
+  ws->len = len;
+  return ws;
+}
+
+LUALIB_API const wchar_t *luaL_convertlstringi (lua_State *L, int idx, size_t *len)
+{
+  size_t nlen;
+  const char *s = lua_tolstring(L, idx, &nlen);
+  return luaL_convertlstring(L, s, nlen, len);
+}
+
+LUALIB_API const char *luaL_convertlwstring (lua_State *L, const wchar_t *ws, size_t wlen, size_t *len)
+{
+  int size;
+  luaL_Buffer buf;
+  char *s;
+  if (ws == NULL) {
+    if (len != NULL) *len = 0;
+    return NULL;
+  }
+  size = WideCharToMultiByte(CP_UTF8, 0, ws, wlen, NULL, 0, NULL, NULL);
+  if (size == 0) { /* conversion failure */
+    if (len != NULL) *len = 0;
+    return NULL;
+  }
+  s = luaL_buffinitsize(L, &buf, size);
+  WideCharToMultiByte(CP_UTF8, 0, ws, wlen, s, size, NULL, NULL);
+  luaL_pushresultsize(&buf, size);
+  if (len != NULL) *len = size;
+  return lua_tostring(L, -1);
+}
+
+LUALIB_API const wchar_t *luaL_convertlstring (lua_State *L, const char *s, size_t nlen, size_t *len)
+{
+  int size;
+  UWideString *ws;
+  if (s == NULL) {
+    if (len != NULL) *len = 0;
+    return NULL;
+  }
+  size = MultiByteToWideChar(CP_UTF8, 0, s, nlen, NULL, 0);
+  if (size == 0) { /* conversion failure */
+    if (len != NULL) *len = 0;
+    return NULL;
+  }
+
+  ws = newwidestr(L, NULL, size);
+  MultiByteToWideChar(CP_UTF8, 0, s, nlen, ws->s, size);
+  if (len != NULL) *len = size;
+  return ws->s;
+}
+
+LUALIB_API const char *(luaL_convertwstring) (lua_State *L, const wchar_t *ws, size_t *len)
+{
+  int size, wlen;
+  luaL_Buffer buf;
+  char *s;
+  if (ws == NULL) {
+    if (len != NULL) *len = 0;
+    return NULL;
+  }
+  wlen = wcslen(ws);
+  size = WideCharToMultiByte(CP_UTF8, 0, ws, wlen, NULL, 0, NULL, NULL);
+  if (size == 0) { /* conversion failure */
+    if (len != NULL) *len = 0;
+    return NULL;
+  }
+  s = luaL_buffinitsize(L, &buf, size);
+  WideCharToMultiByte(CP_UTF8, 0, ws, wlen, s, size, NULL, NULL);
+  luaL_pushresultsize(&buf, size);
+  if (len != NULL) *len = size;
+  return lua_tostring(L, -1);
+}
+
+LUALIB_API const wchar_t *luaL_checkconvertlstring (lua_State *L, int idx, size_t *len)
+{
+  size_t nlen;
+  const char *s = luaL_checklstring(L, idx, &nlen);
+  const wchar_t *ws = luaL_convertlstring(L, s, nlen, len);
+  if (ws == NULL) luaL_error(L, "conversion failure");
+  return ws;
+}
+
+LUALIB_API const wchar_t *luaL_optconvertlstring (lua_State *L, int idx, const wchar_t *def, size_t *len)
+{
+  if (lua_isnoneornil(L, idx)) {
+    if (len != NULL) *len = (def ? wcslen(def) : 0);
+    return def;
+  }
+  return luaL_checkconvertlstring(L, idx, len);
+}
+
+#endif
+/* }====================================================== */

contrib/lua/src/lauxlib.h
@@ -106,6 +106,8 @@ LUALIB_API void (luaL_traceback) (lua_State *L, lua_State *L1,
 LUALIB_API void (luaL_requiref) (lua_State *L, const char *modname,
                                  lua_CFunction openf, int glb);
 
+LUALIB_API const char *(luaL_getenv) (lua_State *L, const char *name);
+
 /*
 ** ===============================================================
 ** some useful macros
@@ -257,6 +259,32 @@ LUALIB_API void (luaL_openlib) (lua_State *L, const char *libname,
 #endif
 /* }============================================================ */
 
+
+/*
+** {============================================================
+** Lua wide-string support (Windows only)
+** =============================================================
+*/
+#if defined(LUA_USE_WINDOWS)
+/* converts a UTF-8 Lua string at `idx` to a wide string */
+LUALIB_API const wchar_t *(luaL_convertlstringi) (lua_State *L, int idx, size_t *len);
+/* converts a wide string at `idx` to a UTF-8 Lua string */
+LUALIB_API const char *(luaL_convertlwstring) (lua_State *L, const wchar_t *ws, size_t wlen, size_t *len);
+/* converts a UTF-8 Lua string at `idx` to a wide string */
+LUALIB_API const wchar_t *(luaL_convertlstring) (lua_State *L, const char *s, size_t nlen, size_t *len);
+/* converts a UTF-8 Lua string at `idx` to a wide string */
+LUALIB_API const wchar_t *(luaL_checkconvertlstring) (lua_State *L, int idx, size_t *len);
+/* converts a UTF-8 Lua string at `idx` to a wide string */
+LUALIB_API const wchar_t *(luaL_optconvertlstring) (lua_State *L, int idx, const wchar_t *def, size_t *len);
+
+LUALIB_API const char *(luaL_convertwstring) (lua_State *l, const wchar_t *ws, size_t *len);
+
+#define luaL_convertstringi(L,i) (luaL_convertlstringi(L, (i), NULL))
+#define luaL_convertstring(L,s) (luaL_convertlstring(L, (s), (s ? strlen(s) : 0), NULL))
+#define luaL_checkconvertstring(L,i) (luaL_checkconvertlstring(L, (i), NULL))
+#define luaL_optconvertstring(L,i,d) (luaL_optconvertlstring(L, (i), (d), NULL))
+#endif
+/* }============================================================ */
 
 
 #endif

contrib/lua/src/liolib.c
@@ -25,7 +25,7 @@
 /*
 * PREMAKE change: UTF-8 character support on windows.
 */
-#if defined(LUA_WIN)
+#if defined(LUA_USE_WINDOWS)  /* PREMAKE: UTF-8 support on Windows */
 #include <windows.h>
 #endif
 
@@ -253,7 +253,14 @@ static LStream *newfile (lua_State *L) {
 
 static void opencheck (lua_State *L, const char *fname, const char *mode) {
   LStream *p = newfile(L);
+#if defined(LUA_USE_WINDOWS)  /* PREMAKE: UTF-8 support on Windows */
+  const wchar_t *wfname = luaL_convertstring(L, fname), *wmode = luaL_convertstring(L, mode);
+  if (wfname == NULL || wmode == NULL) luaL_error(L, "encoding error");
+  p->f = _wfopen(wfname, wmode);
+  lua_pop(L, 2); /* pop converted strings */
+#else
   p->f = fopen(fname, mode);
+#endif
   if (p->f == NULL)
     luaL_error(L, "cannot open file '%s' (%s)", fname, strerror(errno));
 }
@@ -265,26 +272,14 @@ static int io_open (lua_State *L) {
   LStream *p = newfile(L);
   const char *md = mode;  /* to traverse/check mode */
   luaL_argcheck(L, l_checkmode(md), 2, "invalid mode");
-
-  /*
-   * PREMAKE change: UTF-8 character support on windows.
-   */
-#if defined(LUA_WIN)
-  wchar_t wide_path[4096];
-  if (MultiByteToWideChar(CP_UTF8, 0, filename, -1, wide_path, 4096) == 0)
+#if defined(LUA_USE_WINDOWS)  /* PREMAKE: UTF-8 support on Windows */
   {
-    lua_pushstring(L, "unable to encode path");
-    return lua_error(L);
+    const wchar_t *wfilename = luaL_convertstring(L, filename), *wmode = luaL_convertstring(L, md);
+    if (wfilename == NULL || wmode == NULL)
+      return (errno = EINVAL, luaL_fileresult(L, 0, filename)); /* encoding error */
+    p->f = _wfopen(wfilename, wmode);
+    lua_pop(L, 2); /* pop converted strings */
   }
-
-  wchar_t wide_mode[64];
-  if (MultiByteToWideChar(CP_UTF8, 0, mode, -1, wide_mode, 64) == 0)
-  {
-    lua_pushstring(L, "unable to encode open mode");
-    return lua_error(L);
-  }
-
-  p->f = _wfopen(wide_path, wide_mode);
 #else
   p->f = fopen(filename, mode);
 #endif
@@ -303,12 +298,21 @@ static int io_pclose (lua_State *L) {
 
 
 static int io_popen (lua_State *L) {
+#if defined(LUA_USE_WINDOWS)
+  const wchar_t* wmode = luaL_optconvertstring(L, 2, L"r");
+  const wchar_t *wfilename = luaL_checkconvertstring(L, 1);
+  LStream* p = newprefile(L);
+  p->f = _wpopen(wfilename, wmode);
+  p->closef = &io_pclose;
+  return (p->f == NULL) ? luaL_fileresult(L, 0, lua_tostring(L, 1)) : 1;
+#else
   const char *filename = luaL_checkstring(L, 1);
   const char *mode = luaL_optstring(L, 2, "r");
   LStream *p = newprefile(L);
   p->f = l_popen(L, filename, mode);
   p->closef = &io_pclose;
   return (p->f == NULL) ? luaL_fileresult(L, 0, filename) : 1;
+#endif
 }
 
 

commit 489a72f3717b213ab8dc75429f84c7c5c807082f

fix return value of os.writefile_ifnotequal()
- Fix the regression introduced in #2654
- Update the document to match the long-standing existing behavior

src/host/os_writefile_ifnotequal.c
@@ -105,7 +105,7 @@ int os_writefile_ifnotequal(lua_State* L)
 
 		if (!error)
 		{
-			lua_pushinteger(L, error ? -1 : 0);
+			lua_pushinteger(L, 1);
 			return 1;
 		}
 	}